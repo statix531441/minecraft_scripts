a2238af5e2eddf7a63767ee44705e4728f520e60
rusty_tendrils <sumadithya@gmail.com>
2026-05-03 20:01:29 +0530

    Made the flake generic (not just linux specific)

--- flake.nix
+++ flake.nix
@@ -3,17 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        python3
-        openjdk21
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in 
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+            openjdk21
+          ];
+        };
+      }
+    );
 }
